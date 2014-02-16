5baf51a59a32e14485603747c81a91dc7c27d36b
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2014-02-16 02:56:20 -0600

    add unique filter

--- app/scripts/filters/unique.js
+++ app/scripts/filters/unique.js
@@ -2,16 +2,38 @@
 
 angular.module('myApp')
 
-  .filter('unique', function() {
-    return function(input, key) {
-      var unique = {};
-      var uniqueList = [];
-      for(var i = 0; i < input.length; i++){
-        if(typeof unique[input[i][key]] === 'undefined'){
-          unique[input[i][key]] = '';
-          uniqueList.push(input[i]);
+  .filter('unique', ['$parse', function($parse) {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var uniqueItems = [],
+        get = angular.isString(filterOn) ? $parse(filterOn) : function (item) { return item; };
+
+      var extractValueToCompare = function (item) {
+        return angular.isObject(item) ? get(item) : item;
+      };
+
+      angular.forEach(items, function (item) {
+        var isDuplicate = false;
+
+        for (var i = 0; i < uniqueItems.length; i++) {
+          if (angular.equals(extractValueToCompare(uniqueItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
         }
-      }
-      return uniqueList;
-    };
-  });
+        if (!isDuplicate) {
+          uniqueItems.push(item);
+        }
+
+      });
+      items = uniqueItems;
+    }
+    return items;
+  };
+}]);
